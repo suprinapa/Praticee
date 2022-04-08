02c165d544165a47106772022f294887f8572b43
Suprina Paudel <57934428+suprinapa@users.noreply.github.com>
2022-04-08 20:41:54 +0545

    Add files via upload

--- grails-app/services/pratice/MemberService.groovy
+++ grails-app/services/pratice/MemberService.groovy
@@ -6,30 +6,76 @@ import groovy.sql.Sql
 @Transactional
 class MemberService {
 
-/*    def create(params){
-        // Creating a connection to the database
+    def connection() {
         def sql = Sql.newInstance('jdbc:mysql://localhost:3306/myapp', 'root',
                 'root', 'com.mysql.jdbc.Driver')
+        sql.connection.setAutoCommit(false)
+        return sql;
+    }
 
-        sql.connection.autoCommit = false
-
-        int id = 1
-        BigInteger version =20
+    def create() {
+        def connection =  connection()
+        BigInteger version = 21
         String address = "Ktm"
-        String name = "Mac"
+        String name = "Dai"
 
-        def sqlStr = "INSERT INTO member(id,version,address,name) VALUES (${id}, ${version}, ${address}, ${name})"
+      /*  List<Member> list = new ArrayList<>();
 
+        list.add(version)
+        list.add(address)
+        list.add(name)
+        for(int i= 0; i < list.size(); i++){
+
+        }*/
+
+       def sqlStr = "INSERT INTO member(version,address,name) VALUES (${version}, ${address}, ${name})"
         try {
-            sql.execute(sqlStr);
-            sql.commit()
+            connection.execute(sqlStr);
+            connection.commit()
             println("Successfully committed")
-        } catch(Exception ex) {
-            sql.rollback()
-            log.error("---------" +ex.getMessage())
+        } catch (Exception ex) {
+            connection.rollback()
+            log.error("---------" + ex.getMessage())
             println("rollback")
         }
-        sql.close()
-    }*/
+       connection.close()
+    }
 
+    def getData() {
+        def connection =  connection()
+        connection.eachRow('select * from member') {
+            tp ->
+                println([tp.id, tp.version, tp.address, tp.name])
+        }
+        connection.close()
+    }
+
+    def update(){
+        def connection =  connection()
+            def sqlStr = "UPDATE member SET id = id + 6 WHERE address = 'Ktm'"
+
+            try {
+                connection.execute(sqlStr);
+                connection.commit()
+                println("Successfully committed")
+            }catch(Exception ex) {
+                connection.rollback()
+                println("Transaction rollback")
+            }
+            connection.close()
+        }
+
+    def delete() {
+        def connection =  connection()
+        def sqlStr = "DELETE FROM member WHERE name='Dai'"
+        try {
+            connection.execute(sqlStr);
+            connection.commit()
+            println("Successfully committed")
+        } catch (Exception ex) {
+            connection.rollback()
+            println("Transaction rollback")
+        }
+        connection.close()
+    }
 }
